1d26d669c5fcc320aff2e9ed4068deddb90d26fb
Andrey Arthur <andrey_arthur1@outlook.com>
2024-02-18 13:44:06 -0300

    docs: document the main lib methods

--- lib/index.ts
+++ lib/index.ts
@@ -3,29 +3,44 @@ import path from 'path';
 import Runner from './runner';
 import Logger from './logger';
 
+/**
+ * Declares a new test runner
+ *
+ * @returns a runner instance
+ */
+const runner = (): Runner => new Runner(new Logger());
+
+/**
+ * Gets all files in a directory, filtered by a condition
+ *
+ * @param baseDirectory the base search directory
+ * @param conditionCallback a filter callback for searched files
+ */
+const files = (
+  baseDirectory: string,
+  conditionCallback: (file: string) => boolean,
+): string[] => {
+  const recursive = (directory: string): string[] => {
+    let filesList: string[] = [];
+    const items: string[] = fs.readdirSync(directory);
+    items.forEach((item) => {
+      const itemPath: string = path.join(directory, item);
+      if (fs.statSync(itemPath).isDirectory()) {
+        filesList = filesList.concat(recursive(itemPath));
+      } else {
+        filesList.push(itemPath);
+      }
+    });
+    return filesList;
+  };
+  const filesInDirectory: string[] = recursive(baseDirectory);
+  return filesInDirectory.filter(conditionCallback);
+};
+
 const justest = {
-  runner: (): Runner => new Runner(new Logger()),
+  runner,
   helpers: {
-    files(
-      baseDirectory: string,
-      conditionCallback: (file: string) => boolean,
-    ): string[] {
-      const recursive = (directory: string): string[] => {
-        let filesList: string[] = [];
-        const items: string[] = fs.readdirSync(directory);
-        items.forEach((item) => {
-          const itemPath: string = path.join(directory, item);
-          if (fs.statSync(itemPath).isDirectory()) {
-            filesList = filesList.concat(recursive(itemPath));
-          } else {
-            filesList.push(itemPath);
-          }
-        });
-        return filesList;
-      };
-      const filesInDirectory: string[] = recursive(baseDirectory);
-      return filesInDirectory.filter(conditionCallback);
-    },
+    files,
   },
 };
 
